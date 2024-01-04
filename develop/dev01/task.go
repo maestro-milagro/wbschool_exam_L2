908491e4dd705d18f15fc0a98613a8ccbd93b5ba
Sergey Korchagin <millagros77@gmail.com>
2024-01-04 18:32:34 +0300

    many changes 2

--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"time"
+)
+
 /*
 === Базовая задача ===
 
@@ -13,5 +19,19 @@ package main
 */
 
 func main() {
+	// ntpTime, err := ntp.Time("pool.ntp.org")
+	// time.apple.com will give the same result as pool.ntp.org
+	ntpTime, err := ntp.Time("time.apple.com")
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	ntpTimeFormatted := ntpTime.Format(time.UnixDate)
+
+	fmt.Printf("Network time: %v\n", ntpTime)
+	fmt.Printf("Unix Date Network time: %v\n", ntpTimeFormatted)
+	fmt.Println("+++++++++++++++++++++++++++++++")
+	timeFormatted := time.Now().Local().Format(time.UnixDate)
+	fmt.Printf("System time: %v\n", time.Now())
+	fmt.Printf("Unix Date System time: %v\n", timeFormatted)
 }
--- develop/dev11/task.go
+++ develop/dev11/task.go
@@ -1,14 +1,28 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
 /*
 === HTTP server ===
 
 Реализовать HTTP сервер для работы с календарем. В рамках задания необходимо работать строго со стандартной HTTP библиотекой.
 В рамках задания необходимо:
-	1. Реализовать вспомогательные функции для сериализации объектов доменной области в JSON.
-	2. Реализовать вспомогательные функции для парсинга и валидации параметров методов /create_event и /update_event.
-	3. Реализовать HTTP обработчики для каждого из методов API, используя вспомогательные функции и объекты доменной области.
-	4. Реализовать middleware для логирования запросов
+ 1. Реализовать вспомогательные функции для сериализации объектов доменной области в JSON.
+ 2. Реализовать вспомогательные функции для парсинга и валидации параметров методов /create_event и /update_event.
+ 3. Реализовать HTTP обработчики для каждого из методов API, используя вспомогательные функции и объекты доменной области.
+ 4. Реализовать middleware для логирования запросов
+
 Методы API: POST /create_event POST /update_event POST /delete_event GET /events_for_day GET /events_for_week GET /events_for_month
 Параметры передаются в виде www-url-form-encoded (т.е. обычные user_id=3&date=2019-09-09).
 В GET методах параметры передаются через queryString, в POST через тело запроса.
@@ -16,12 +30,334 @@ package main
 либо {"error": "..."} в случае ошибки бизнес-логики.
 
 В рамках задачи необходимо:
-	1. Реализовать все методы.
-	2. Бизнес логика НЕ должна зависеть от кода HTTP сервера.
-	3. В случае ошибки бизнес-логики сервер должен возвращать HTTP 503. В случае ошибки входных данных (невалидный int например) сервер должен возвращать HTTP 400. В случае остальных ошибок сервер должен возвращать HTTP 500. Web-сервер должен запускаться на порту указанном в конфиге и выводить в лог каждый обработанный запрос.
-	4. Код должен проходить проверки go vet и golint.
+ 1. Реализовать все методы.
+ 2. Бизнес логика НЕ должна зависеть от кода HTTP сервера.
+ 3. В случае ошибки бизнес-логики сервер должен возвращать HTTP 503. В случае ошибки входных данных (невалидный int например) сервер должен возвращать HTTP 400. В случае остальных ошибок сервер должен возвращать HTTP 500. Web-сервер должен запускаться на порту указанном в конфиге и выводить в лог каждый обработанный запрос.
+ 4. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type Task struct {
+	Id   int       `json:"id"`
+	Text string    `json:"text"`
+	Tags []string  `json:"tags"`
+	Due  time.Time `json:"due"`
+}
 
+// TaskStore is a simple in-memory database of tasks; TaskStore methods are
+// safe to call concurrently.
+type TaskStore struct {
+	sync.Mutex
+
+	tasks  map[int]Task
+	nextId int
+}
+
+func New() *TaskStore {
+	ts := &TaskStore{}
+	ts.tasks = make(map[int]Task)
+	ts.nextId = 0
+	return ts
+}
+
+// CreateTask creates a new task in the store.
+func (ts *TaskStore) CreateTask(text string, tags []string, due time.Time) int {
+	ts.Lock()
+	defer ts.Unlock()
+
+	task := Task{
+		Id:   ts.nextId,
+		Text: text,
+		Due:  due}
+	task.Tags = make([]string, len(tags))
+	copy(task.Tags, tags)
+
+	ts.tasks[ts.nextId] = task
+	ts.nextId++
+	return task.Id
+}
+
+// GetTask retrieves a task from the store, by id. If no such id exists, an
+// error is returned.
+func (ts *TaskStore) GetTask(id int) (Task, error) {
+	ts.Lock()
+	defer ts.Unlock()
+
+	t, ok := ts.tasks[id]
+	if ok {
+		return t, nil
+	} else {
+		return Task{}, fmt.Errorf("task with id=%d not found", id)
+	}
+}
+
+// DeleteTask deletes the task with the given id. If no such id exists, an error
+// is returned.
+func (ts *TaskStore) DeleteTask(id int) error {
+	ts.Lock()
+	defer ts.Unlock()
+
+	if _, ok := ts.tasks[id]; !ok {
+		return fmt.Errorf("task with id=%d not found", id)
+	}
+
+	delete(ts.tasks, id)
+	return nil
+}
+
+// DeleteAllTasks deletes all tasks in the store.
+func (ts *TaskStore) DeleteAllTasks() error {
+	ts.Lock()
+	defer ts.Unlock()
+
+	ts.tasks = make(map[int]Task)
+	return nil
+}
+
+// GetAllTasks returns all the tasks in the store, in arbitrary order.
+func (ts *TaskStore) GetAllTasks() []Task {
+	ts.Lock()
+	defer ts.Unlock()
+
+	allTasks := make([]Task, 0, len(ts.tasks))
+	for _, task := range ts.tasks {
+		allTasks = append(allTasks, task)
+	}
+	return allTasks
+}
+
+// GetTasksByTag returns all the tasks that have the given tag, in arbitrary
+// order.
+func (ts *TaskStore) GetTasksByTag(tag string) []Task {
+	ts.Lock()
+	defer ts.Unlock()
+
+	var tasks []Task
+
+taskloop:
+	for _, task := range ts.tasks {
+		for _, taskTag := range task.Tags {
+			if taskTag == tag {
+				tasks = append(tasks, task)
+				continue taskloop
+			}
+		}
+	}
+	return tasks
+}
+
+// GetTasksByDueDate returns all the tasks that have the given due date, in
+// arbitrary order.
+func (ts *TaskStore) GetTasksByDueDate(year int, month time.Month, day int) []Task {
+	ts.Lock()
+	defer ts.Unlock()
+
+	var tasks []Task
+
+	for _, task := range ts.tasks {
+		y, m, d := task.Due.Date()
+		if y == year && m == month && d == day {
+			tasks = append(tasks, task)
+		}
+	}
+
+	return tasks
+}
+
+type taskServer struct {
+	store *TaskStore
+}
+
+func NewTaskServer() *taskServer {
+	store := New()
+	return &taskServer{store: store}
+}
+
+// renderJSON renders 'v' as JSON and writes it as a response into w.
+func renderJSON(w http.ResponseWriter, v interface{}) {
+	js, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (ts *taskServer) taskHandler(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path == "/task/" {
+		// Request is plain "/task/", without trailing ID.
+		if req.Method == http.MethodPost {
+			ts.createTaskHandler(w, req)
+		} else if req.Method == http.MethodGet {
+			ts.getAllTasksHandler(w, req)
+		} else if req.Method == http.MethodDelete {
+			ts.deleteAllTasksHandler(w, req)
+		} else {
+			http.Error(w, fmt.Sprintf("expect method GET, DELETE or POST at /task/, got %v", req.Method), http.StatusMethodNotAllowed)
+			return
+		}
+	} else {
+		// Request has an ID, as in "/task/<id>".
+		path := strings.Trim(req.URL.Path, "/")
+		pathParts := strings.Split(path, "/")
+		if len(pathParts) < 2 {
+			http.Error(w, "expect /task/<id> in task handler", http.StatusBadRequest)
+			return
+		}
+		id, err := strconv.Atoi(pathParts[1])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if req.Method == http.MethodDelete {
+			ts.deleteTaskHandler(w, req, id)
+		} else if req.Method == http.MethodGet {
+			ts.getTaskHandler(w, req, id)
+		} else {
+			http.Error(w, fmt.Sprintf("expect method GET or DELETE at /task/<id>, got %v", req.Method), http.StatusMethodNotAllowed)
+			return
+		}
+	}
+}
+
+func (ts *taskServer) createTaskHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling task create at %s\n", req.URL.Path)
+
+	// Types used internally in this handler to (de-)serialize the request and
+	// response from/to JSON.
+	type RequestTask struct {
+		Text string    `json:"text"`
+		Tags []string  `json:"tags"`
+		Due  time.Time `json:"due"`
+	}
+
+	type ResponseId struct {
+		Id int `json:"id"`
+	}
+
+	// Enforce a JSON Content-Type.
+	contentType := req.Header.Get("Content-Type")
+	mediatype, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if mediatype != "application/json" {
+		http.Error(w, "expect application/json Content-Type", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	dec := json.NewDecoder(req.Body)
+	dec.DisallowUnknownFields()
+	var rt RequestTask
+	if err := dec.Decode(&rt); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := ts.store.CreateTask(rt.Text, rt.Tags, rt.Due)
+	renderJSON(w, ResponseId{Id: id})
+}
+
+func (ts *taskServer) getAllTasksHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling get all tasks at %s\n", req.URL.Path)
+
+	allTasks := ts.store.GetAllTasks()
+	renderJSON(w, allTasks)
+}
+
+func (ts *taskServer) getTaskHandler(w http.ResponseWriter, req *http.Request, id int) {
+	log.Printf("handling get task at %s\n", req.URL.Path)
+
+	task, err := ts.store.GetTask(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	renderJSON(w, task)
+}
+
+func (ts *taskServer) deleteTaskHandler(w http.ResponseWriter, req *http.Request, id int) {
+	log.Printf("handling delete task at %s\n", req.URL.Path)
+
+	err := ts.store.DeleteTask(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+	}
+}
+
+func (ts *taskServer) deleteAllTasksHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling delete all tasks at %s\n", req.URL.Path)
+	ts.store.DeleteAllTasks()
+}
+
+func (ts *taskServer) tagHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling tasks by tag at %s\n", req.URL.Path)
+
+	if req.Method != http.MethodGet {
+		http.Error(w, fmt.Sprintf("expect method GET /tag/<tag>, got %v", req.Method), http.StatusMethodNotAllowed)
+		return
+	}
+
+	path := strings.Trim(req.URL.Path, "/")
+	pathParts := strings.Split(path, "/")
+	if len(pathParts) < 2 {
+		http.Error(w, "expect /tag/<tag> path", http.StatusBadRequest)
+		return
+	}
+	tag := pathParts[1]
+
+	tasks := ts.store.GetTasksByTag(tag)
+	renderJSON(w, tasks)
+}
+
+func (ts *taskServer) dueHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling tasks by due at %s\n", req.URL.Path)
+
+	if req.Method != http.MethodGet {
+		http.Error(w, fmt.Sprintf("expect method GET /due/<date>, got %v", req.Method), http.StatusMethodNotAllowed)
+		return
+	}
+
+	path := strings.Trim(req.URL.Path, "/")
+	pathParts := strings.Split(path, "/")
+
+	badRequestError := func() {
+		http.Error(w, fmt.Sprintf("expect /due/<year>/<month>/<day>, got %v", req.URL.Path), http.StatusBadRequest)
+	}
+	if len(pathParts) != 4 {
+		badRequestError()
+		return
+	}
+
+	year, err := strconv.Atoi(pathParts[1])
+	if err != nil {
+		badRequestError()
+		return
+	}
+	month, err := strconv.Atoi(pathParts[2])
+	if err != nil || month < int(time.January) || month > int(time.December) {
+		badRequestError()
+		return
+	}
+	day, err := strconv.Atoi(pathParts[3])
+	if err != nil {
+		badRequestError()
+		return
+	}
+
+	tasks := ts.store.GetTasksByDueDate(year, time.Month(month), day)
+	renderJSON(w, tasks)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	server := NewTaskServer()
+	mux.HandleFunc("/task/", server.taskHandler)
+	mux.HandleFunc("/tag/", server.tagHandler)
+	mux.HandleFunc("/due/", server.dueHandler)
+
+	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("SERVERPORT"), mux))
 }
